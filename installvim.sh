cc51773221630d13ca559a9d89937a0f285167e1
avriltank <chenfa1@qq.com>
2020-11-14 22:11:08 +0800

    source无效解决

--- installvim.sh
+++ installvim.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #cd /
 #git clone https://github.com/avriltank/neovimplugin
 
@@ -7,20 +8,23 @@ sudo yum install ripgrep
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-source ~/.bashrc
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/pathogen.vim --create-dirs \
        https://tpo.pe/pathogen.vim'
 
+
+
+chmod 777 /neovimplugin/nvim
+echo 'export PATH=$PATH:/neovimplugin' >> /etc/profile
+
+mkdir -p ~/.config/nvim
+cp /neovimplugin/init.vim  ~/.config/nvim/init.vim
+
 yum --enablerepo=epel -y install fuse-sshfs # install from EPEL
 user="$(whoami)"
 usermod -a -G fuse "$user" 
 
-chmod 777 /neovimplugin/nvim
-echo 'export PATH=$PATH:/neovimplugin' >> /etc/profile
+source ~/.bashrc
 source /etc/profile
-
-mkdir -p ~/.config/nvim
-cp /neovimplugin/init.vim  ~/.config/nvim/init.vim

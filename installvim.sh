1babea69822e924179e57a961320497d31b30f20
avriltank <chenfa1@qq.com>
2020-11-14 21:03:43 +0800

    安装neovim直接用nvim二进制程序

--- installvim.sh
+++ installvim.sh
@@ -4,8 +4,6 @@
 sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 sudo yum install ripgrep
 
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y neovim python36-neovim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'

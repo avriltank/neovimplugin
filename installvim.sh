687d78fb804961ac17303a5bd098e0305fb21863
avriltank <chenfa1@qq.com>
2020-11-14 21:25:34 +0800

    追加环境变量bug

--- installvim.sh
+++ installvim.sh
@@ -9,9 +9,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/pathogen.vim --create-dirs \
        https://tpo.pe/pathogen.vim'
-chmod 777 /neovim/nvim
-echo 'export PATH=$PATH:/neovimplugin' >> /etc/profile
-source /etc/profile
+/neovimplugin/nvim --appimage-extract
+./squashfs-root/usr/bin/nvim
 
 mkdir -p ~/.config/nvim
 cp /neovimplugin/init.vim  ~/.config/nvim/init.vim

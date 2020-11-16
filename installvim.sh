01e7873dd669c573acdb807d4dce069047527bb3
avriltank <chenfa1@qq.com>
2020-11-16 23:39:03 +0800

    centos source add

--- installvim.sh
+++ installvim.sh
@@ -3,6 +3,7 @@
 #git clone https://github.com/avriltank/neovimplugin
 
 sudo yum -y install yum-utils
+yum install -y epel-release
 sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 sudo yum install ripgrep
 
